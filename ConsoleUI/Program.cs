9a445c7882cec814c622be75f5a9290b938411f2
burcu arslan <76595617+burcuarslan@users.noreply.github.com>
2021-02-13 17:12:48 +0300

    03.02.21

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            //foreach (var product in productManager.GetAll())
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            foreach (var product in productManager.GetByUnitPrice(50,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
